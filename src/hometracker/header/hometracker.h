a306dd91d8ca9113621faa965c6616f19ea6bd11
ahmetberacelik <ahmetberacelik@hotmail.com>
2024-05-01 18:02:31 +0300

    Unnecessary codes were deleted.

--- src/hometracker/header/hometracker.h
+++ src/hometracker/header/hometracker.h
@@ -7,56 +7,6 @@
 #ifndef HOMETRACKER_H
 #define HOMETRACKER_H
 
-#include "../../utility/header/commonTypes.h"
 
-namespace Coruh
-{
-    namespace Hometracker
-    {
-        /**
-            @class Hometracker
-            @brief Provides Basic functions for various operations.
-        */
-        class Hometracker
-        {
-        public:
-            /**
-             * Adds two numbers.
-             * @param a First operand.
-             * @param b Second operand.
-             * @return The sum of a and b.
-             */
-            static double add(double a, double b);
-
-            /**
-             * Subtracts the second number from the first.
-             * @param a Minuend.
-             * @param b Subtrahend.
-             * @return The result of a - b.
-             */
-            static double subtract(double a, double b);
-
-            /**
-             * Multiplies two numbers.
-             * @param a First operand.
-             * @param b Second operand.
-             * @return The product of a and b.
-             */
-            static double multiply(double a, double b);
-
-            /**
-             * Divides the first number by the second.
-             * Throws std::invalid_argument if the second number is zero.
-             * @param a Dividend.
-             * @param b Divisor.
-             * @return The result of a / b.
-             * @throws std::invalid_argument If b is zero.
-             */
-            static double divide(double a, double b);
-
-
-        };
-    }
-}
 
 #endif // HOMETRACKER_H
--- src/tests/hometracker/hometracker_test.cpp
+++ src/tests/hometracker/hometracker_test.cpp
@@ -3,8 +3,6 @@
 #include "gtest/gtest.h"
 #include "../../hometracker/header/hometracker.h"  // Adjust this include path based on your project structure
 
-using namespace Coruh::Hometracker;
-
 class HometrackerTest : public ::testing::Test {
 protected:
 	void SetUp() override {
@@ -15,30 +13,6 @@ protected:
 		// Clean up test data
 	}
 };
-
-TEST_F(HometrackerTest, TestAdd) {
-	double result = Hometracker::add(5.0, 3.0);
-	EXPECT_DOUBLE_EQ(result, 8.0);
-}
-
-TEST_F(HometrackerTest, TestSubtract) {
-	double result = Hometracker::subtract(5.0, 3.0);
-	EXPECT_DOUBLE_EQ(result, 2.0);
-}
-
-TEST_F(HometrackerTest, TestMultiply) {
-	double result = Hometracker::multiply(5.0, 3.0);
-	EXPECT_DOUBLE_EQ(result, 15.0);
-}
-
-TEST_F(HometrackerTest, TestDivide) {
-	double result = Hometracker::divide(6.0, 3.0);
-	EXPECT_DOUBLE_EQ(result, 2.0);
-}
-
-TEST_F(HometrackerTest, TestDivideByZero) {
-	EXPECT_THROW(Hometracker::divide(5.0, 0.0), std::invalid_argument);
-}
 
 /**
  * @brief The main function of the test program.
